96a91eda945aa9541384852651a22e438ff7e760
thezapalsky <apap549@gmail.com>
2022-05-24 15:08:46 +0200

    ???

--- p3/src/p3/Main.java
+++ p3/src/p3/Main.java
@@ -1,7 +1,12 @@
 package p3;
 
+import com.sun.source.tree.Tree;
+
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.PriorityQueue;
+import java.util.Set;
 
 public class Main {
 
@@ -11,30 +16,86 @@ public class Main {
         String pth =  "input2.txt";
         Reader rdr = new Reader(pth);
         Graph g = rdr.readFile();
-        System.out.println(g);
-
-        //minheap stuff
-        // instead of implementing minHeap class from scratch I'm using PQ from Java's API
-        PriorityQueue<Edge> minHeap = new PriorityQueue<>(
-                g.edges().size(),
-                Edge::compareTo);
-        //System.out.println(minHeap.comparator());
-
-        for (Edge e:g.edges() ) {
-            //System.out.print(e);
-            minHeap.add(e);
-        }
-        System.out.print("\n");
-        while(minHeap.peek()!=null){
-            System.out.print(minHeap.poll());
-        }
-
+        //System.out.println(g);
 
         //Kruskal stuff
-        //todo
+        Graph t = MSTKruskal(g);
 
         //canvas stuff
         //Canvas.paint(g);
+        //Canvas.paint(g,t);
 
+    }
+
+    static Graph MSTKruskal(Graph g){
+        // ?? //todo
+        // for every edge create a one element graph
+        // not graph, cause it does not implement unions?
+        // start to union the graphs
+
+        Graph t = new Graph();
+
+        HashMap<String,Set> forest = new HashMap<>();
+        for (Vertex v : g.vertices()){
+            Set tree = new HashSet();
+            tree.add(v);
+            forest.put(v.toString(),tree);
+        }
+
+        // instead of implementing minHeap class from scratch I'm using PQ from Java's API
+        PriorityQueue<Edge> edges = new PriorityQueue<>(
+                g.edges().size(),
+                Edge::compareTo);
+        for (Edge e:g.edges() ) {
+            edges.add(e);
+        }
+        System.out.println(forest);
+
+        //As long as the set is not empty
+        while(edges.peek()!=null) {
+
+            //The edge with the minimum weight edge is eliminated from the set
+            Edge curr = edges.poll();
+            System.out.print(curr);
+            Vertex u = curr.getU();
+            Vertex v = curr.getV();
+
+            //If the chosen edge connects two different trees it is added to the forest,
+            //combining the two trees into one.
+
+            // 1.
+            for (Set tree : forest.values()) {
+                if (tree.contains(u) ) {
+
+                    //System.out.println("dodaje");
+                    tree.add(v);
+                }
+            }
+        }
+
+//            if(forest.get(u.toString())!=forest.get(v.toString())){
+//                //System.out.print("dupa");
+//                for (Set tree:forest.values()){
+//                    if(tree.contains(u)){
+//                        tree.add(v); // union /AAAAA //todo
+//                        System.out.println("dodaje");
+//                        forest.remove(v.toString()); // remove 1-element v set
+//                        t.add(u.x,u.y, v.x, v.y, curr.getW());
+//                    }
+//                }
+//            }
+//        }
+//        System.out.println("\n"+forest);
+
+            // nwm :(
+//            for (Set tree:forest.values()) {
+//                if(tree.contains(u) && !tree.contains(v)){
+//                    tree.add(v);
+//                    forest.remove(v.toString());
+//                    // add to the t?
+//                    t.add(u.x,u.y, v.x, v.y, curr.getW());
+//                }
+//            }
+        return t;//
     }
 }
